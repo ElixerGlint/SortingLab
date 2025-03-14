9cd71734e57326c42f775b31fa647b2bdf87dbe0
Elixer <everett.rumery@gmail.com>
2025-03-14 14:01:23 -0400

    fasfasf

--- src/Complex.java
+++ src/Complex.java
@@ -15,14 +15,18 @@ public class Complex extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		printbackgroup(g);
-		for(Building b: buildings) {
-			b.drawPiece(g);
-		}
+		//for(Building b: buildings) {
+		//	b.drawPiece(g);
+		//}
 	}
 
 	public void printbackgroup(Graphics g) {
 		g.setColor(Color.gray);
 		g.fillRect(0, 650, 1000, 150);
+		g.setColor(Color.black);
+		for(int i = 0; i < 32; i++) {
+			g.fillRect(30*i + 20, 700, 15, 5);
+		}
 	}
 
 }
