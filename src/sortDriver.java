fa71708c8437bb74fde5d5d941a293ea276d4781
Elixer <everett.rumery@gmail.com>
2025-03-24 20:32:07 -0400

    agsgasg

--- src/sortDriver.java
+++ src/sortDriver.java
@@ -15,7 +15,7 @@ public class sortDriver {
 		frame.setSize(1000, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//JOptionPane for dialogue NO SYSOUT
-		Piece table = new Piece(10);
+		Piece table = new Piece(25);
 		frame.add(table);
 
 		frame.setVisible(true);
